ffacdd40bfd16373dc522702b06a88be35dfb84d
Saralrat <saralrats@ecosoft.co.th>
2024-11-04 22:23:21 +0700

    remove get_field(item_code)

--- menghua_co/public/js/quotation.js
+++ menghua_co/public/js/quotation.js
@@ -1,15 +1,13 @@
 frappe.ui.form.on("Quotation", {
     refresh(frm) {
-        if (frm.fields_dict["items"].grid.get_field("item_code")) {
-            frm.set_query("item_code", "items", function (doc, cdt, cdn) {
-                return {
-                    filters: {
-                        'custom_allow_standard_type': frm.doc.custom_mh_order_type === "หน้าร้าน" ? 1 : 0,
-                        'custom_allow_custom_type': frm.doc.custom_mh_order_type === "สั่งทำ" ? 1 : 0,
-                    }
-                };
-            });
-        }
+        frm.set_query("item_code", "items", function (doc, cdt, cdn) {
+            return {
+                filters: {
+                    'custom_allow_standard_type': frm.doc.custom_mh_order_type === "หน้าร้าน" ? 1 : 0,
+                    'custom_allow_custom_type': frm.doc.custom_mh_order_type === "สั่งทำ" ? 1 : 0,
+                }
+            };
+        });
     },
     custom_mh_order_type: function(frm) {
         frm.refresh_field("items");

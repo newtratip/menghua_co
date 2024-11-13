9596d32f415285465d2419ff4a425523dd5257c7
Saralrat <saralrats@ecosoft.co.th>
2024-11-13 13:57:15 +0700

    delete button change status and tab more info

--- menghua_co/menghua_co/doctype/manufacturing_order/manufacturing_order.js
+++ menghua_co/menghua_co/doctype/manufacturing_order/manufacturing_order.js
@@ -2,41 +2,13 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Manufacturing Order', {
-    refresh: function(frm) {
-        if (frm.doc.docstatus === 1) {  
-            frm.add_custom_button(__('Change Status'), function() {
-                let status_options = [
-                    { "label": "ร่าง", "value": "ร่าง" },
-                    { "label": "พิมพ์ปก", "value": "พิมพ์ปก" },
-                    { "label": "ระหว่างผลิต", "value": "ระหว่างผลิต" },
-                    { "label": "ห่อพลาสติก", "value": "ห่อพลาสติก" },
-                    { "label": "เข้าคลัง", "value": "เข้าคลัง" },
-                    { "label": "ส่งออก", "value": "ส่งออก" }
-                ];
-
-                let dialog = new frappe.ui.Dialog({
-                    title: __('เลือกสถานะใหม่'),
-                    fields: [
-                        {
-                            fieldtype: 'Select',
-                            label: __('เลือกสถานะ'),
-                            fieldname: 'new_status',
-                            options: status_options.map(function(option) { return option.value; }),
-                            reqd: 1
-                        }
-                    ],
-                    primary_action_label: __('ยืนยัน'),
-                    primary_action: function() {
-                        let selected_status = dialog.get_value('new_status');
-                        frm.set_value('status', selected_status); 
-                        frm.refresh_field('status'); 
-                        frm.save();
-                        dialog.hide(); 
-                    }
-                });
-
-                dialog.show(); 
-            });
-        }
+    customer: function(frm) {
+        frm.set_query('sales_order', function() {
+            return {
+                filters: {
+                    'customer': frm.doc.customer
+                }
+            };
+        });
     }
 });

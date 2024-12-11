271b54577e3f53b19aa7fe2996bf38aa8c8e08c1
Saralrat <saralrats@ecosoft.co.th>
2024-12-11 16:34:26 +0700

    improve code

--- menghua_co/menghua_co/doctype/manufacturing_order/manufacturing_order.py
+++ menghua_co/menghua_co/doctype/manufacturing_order/manufacturing_order.py
@@ -7,10 +7,11 @@ from frappe.model.document import Document
 
 class ManufacturingOrder(Document):
     def before_save(self):
-        if self.docstatus == 0 and self.mh_status == "ยกเลิก":
-            self.mh_status = "ร่าง" 
-        elif self.docstatus == 0 and self.mh_status != "ร่าง":
-            frappe.throw("Invalid MH Status")
+        if self.docstatus == 0:
+            if self.mh_status == "ยกเลิก":
+                self.mh_status = "ร่าง"
+            elif self.mh_status != "ร่าง":
+               frappe.throw("Invalid MH Status")
 
     def before_submit(self):
         if self.mh_status == "ร่าง":
